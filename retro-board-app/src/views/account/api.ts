eab46a126d1b389af74988b574a116e4cfa28465
Antoine Jaussoin <antoine@jaussoin.com>
2021-02-28 17:34:12 +0000

    Simplify account API

--- retro-board-app/src/views/account/api.ts
+++ retro-board-app/src/views/account/api.ts
@@ -1,55 +1,22 @@
 import { Quota } from '@retrospected/common';
-
-const requestConfig: Partial<RequestInit> = {
-  mode: 'cors',
-  cache: 'no-cache',
-  credentials: 'same-origin',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  redirect: 'follow',
-  referrer: 'no-referrer',
-};
+import { fetchGet, fetchPatch } from '../../api/fetch';
 
 export async function getPortalUrl(): Promise<string | null> {
-  const response = await fetch(`/api/stripe/portal`, {
-    method: 'GET',
-    ...requestConfig,
-  });
-  if (response.ok) {
-    const session: { url: string } = await response.json();
-    return session.url;
-  }
-  return null;
+  const response = await fetchGet<{ url: string } | null>(
+    `/api/stripe/portal`,
+    null
+  );
+  return response ? response.url : null;
 }
 
 export async function getMembers(): Promise<string[] | null> {
-  const response = await fetch(`/api/stripe/members`, {
-    method: 'GET',
-    ...requestConfig,
-  });
-  if (response.ok) {
-    const members: string[] = await response.json();
-    return members;
-  }
-  return null;
+  return await fetchGet<string[] | null>(`/api/stripe/members`, null);
 }
 
 export async function getQuota(): Promise<Quota | null> {
-  const response = await fetch(`/api/quota`, {
-    method: 'GET',
-    ...requestConfig,
-  });
-  if (response.ok) {
-    const quota: Quota | null = await response.json();
-    return quota;
-  }
-  return null;
+  return await fetchGet<Quota | null>(`/api/quota`, null);
 }
+
 export async function updateMembers(members: string[]): Promise<void> {
-  await fetch(`/api/stripe/members`, {
-    method: 'PATCH',
-    ...requestConfig,
-    body: JSON.stringify(members),
-  });
+  await fetchPatch(`/api/stripe/members`, members);
 }
