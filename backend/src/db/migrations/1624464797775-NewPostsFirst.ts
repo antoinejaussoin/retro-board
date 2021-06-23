a7f88d72474a0ba7f286fed9184c6c51eeadd1a5
Antoine Jaussoin <antoine@jaussoin.com>
2021-06-23 17:47:53 +0100

    UI Config

--- backend/src/db/migrations/1624464797775-NewPostsFirst.ts
+++ backend/src/db/migrations/1624464797775-NewPostsFirst.ts
@@ -4,8 +4,8 @@ export class NewPostsFirst1624464797775 implements MigrationInterface {
     name = 'NewPostsFirst1624464797775'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "templates" ADD "optionsNewpostsfirst" boolean NOT NULL DEFAULT false`);
-        await queryRunner.query(`ALTER TABLE "sessions" ADD "optionsNewpostsfirst" boolean NOT NULL DEFAULT false`);
+        await queryRunner.query(`ALTER TABLE "templates" ADD "optionsNewpostsfirst" boolean NOT NULL DEFAULT true`);
+        await queryRunner.query(`ALTER TABLE "sessions" ADD "optionsNewpostsfirst" boolean NOT NULL DEFAULT true`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
