d9b0d3135d47023cffd9d3cec01f3c8c177332ad
Antoine Jaussoin <antoine@jaussoin.com>
2021-06-26 11:07:10 +0100

    Translations

--- frontend/src/translations/en.ts
+++ frontend/src/translations/en.ts
@@ -370,16 +370,16 @@ export default {
       },
     },
     subscribeButton: 'Subscribe',
-    payButton: 'Purchase',
+    payButton: 'Select',
     cancelButton: 'Cancel',
     startTrial: '30-day Trial',
   },
   Products: {
     team: 'Perfect for smaller teams, you can select up to 20 colleagues who will be upgraded to a Pro account.',
     unlimited:
-      'If you are a bigger company, you will enjoy an unlimited number of Pro accounts',
+      'If you are a bigger company, you will enjoy an unlimited number of Pro accounts.',
     'self-hosted':
-      'Retrospected on premises, one-time fee and unlimited updates. After payment, a licence key will be sent to your email.',
+      'Retrospected on premises, one-time fee and unlimited updates. Keep total control of your data, for ever.',
     users: (users: number) => `${users} users`,
     unlimited_seats: 'Unlimited',
     month: 'month',
