3cb5b960e0ef71a8bd06c7e715fb26f182f14276
Antoine Jaussoin <antoine@jaussoin.com>
2021-06-26 20:43:40 +0100

    Making frontend env variables optional

--- frontend/docker/frontend-entrypoint.sh
+++ frontend/docker/frontend-entrypoint.sh
@@ -4,10 +4,10 @@ set -eu
 envsubst '${BACKEND_HOST} ${BACKEND_PORT}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
 # Replacing Google Analytics and Sentry IDs. Separator is # because the sentry URL contains a /
-sed -i "s#NO_GA#$GA_ID#g" /usr/share/nginx/html/index.html
-sed -i "s#NO_SENTRY#$SENTRY_URL#g" /usr/share/nginx/html/index.html
-sed -i "s#NO_GIPHY#$GIPHY_API_KEY#g" /usr/share/nginx/html/index.html
-sed -i "s#NO_STRIPE#$STRIPE_KEY#g" /usr/share/nginx/html/index.html
-sed -i "s#NO_DEFAULT_LANGUAGE#$DEFAULT_LANGUAGE#g" /usr/share/nginx/html/index.html
+sed -i "s#NO_GA#${GA_ID:-}#g" /usr/share/nginx/html/index.html
+sed -i "s#NO_SENTRY#${SENTRY_URL:-}#g" /usr/share/nginx/html/index.html
+sed -i "s#NO_GIPHY#${GIPHY_API_KEY:-}#g" /usr/share/nginx/html/index.html
+sed -i "s#NO_STRIPE#${STRIPE_KEY:-}#g" /usr/share/nginx/html/index.html
+sed -i "s#NO_DEFAULT_LANGUAGE#${DEFAULT_LANGUAGE:-en}#g" /usr/share/nginx/html/index.html
 
 exec "$@"
