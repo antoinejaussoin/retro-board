e84ceb0c6c000fff3aebb898bd4c49b2f10d62a9
Antoine Jaussoin <antoine@jaussoin.com>
2021-07-04 19:51:59 +0100

    Improving migration

--- backend/src/build-config.ts
+++ backend/src/build-config.ts
@@ -4,10 +4,15 @@ import fs from 'fs';
 
 async function buildOrmConfig() {
   const config = getOrmConfig();
+  const rootPath = path.resolve(__dirname);
+  const entities = path.resolve(rootPath, 'db', 'entities');
+  const migrations = path.resolve(rootPath, 'db', 'migrations');
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  (config as any).entities = [
-    `${path.resolve(__dirname, 'db', 'entities')}/*.ts`,
-  ];
+  (config as any).entities = [`${entities}/*.js`];
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  (config as any).migrations = [`${migrations}/*.js`];
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  (config as any).cli.migrationsDir = migrations;
   const jsonPath = path.resolve(__dirname, '..', 'ormconfig.json');
   fs.writeFileSync(jsonPath, JSON.stringify(config, null, 2));
 }
