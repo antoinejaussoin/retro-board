041ccf397451356dc6139ea27e8ba866f93678f3
Antoine Jaussoin <antoine@jaussoin.com>
2021-10-22 16:49:52 +0100

    stuff

--- frontend/src/views/account/delete/DeleteModal.tsx
+++ frontend/src/views/account/delete/DeleteModal.tsx
@@ -1,19 +1,98 @@
-import { Dialog, DialogContent } from '@mui/material';
+import { Dashboard, Note, Person, ThumbUpOutlined } from '@mui/icons-material';
+import {
+  Dialog,
+  DialogContent,
+  useMediaQuery,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListSubheader,
+  ListItemText,
+  ListItemSecondaryAction,
+  Switch,
+} from '@mui/material';
+import { noop } from 'lodash';
+import { useState } from 'react';
 
 type DeleteModalProps = {
   onClose: () => void;
 };
 
 export function DeleteModal({ onClose }: DeleteModalProps) {
+  const fullScreen = useMediaQuery('(max-width:600px)');
+  const [deleteSessions, setDeleteSessions] = useState(false);
+  const [deletePosts, setDeletePosts] = useState(false);
+  const [deleteVotes, setDeleteVotes] = useState(false);
   return (
     <Dialog
-      //      fullScreen={fullScreen}
+      fullScreen={fullScreen}
       maxWidth="sm"
       fullWidth
       open
       onClose={onClose}
     >
-      <DialogContent>Content</DialogContent>
+      <DialogContent>
+        <List subheader={<ListSubheader>Choose what to delete</ListSubheader>}>
+          <DeleteItem checked disabled icon={<Person />}>
+            Delete your account and any identities linked to your email
+          </DeleteItem>
+          <DeleteItem
+            checked={deleteSessions}
+            onToggle={setDeleteSessions}
+            icon={<Dashboard />}
+          >
+            Delete all the sessions you created. This will delete all posts and
+            votes related to this session, which means it will impact other
+            people.
+          </DeleteItem>
+          <DeleteItem
+            checked={deletePosts}
+            onToggle={setDeletePosts}
+            icon={<Note />}
+          >
+            Delete all the posts you wrote. They will be deleted from all
+            sessions, including sessions you did not create.
+          </DeleteItem>
+          <DeleteItem
+            checked={deleteVotes}
+            onToggle={setDeleteVotes}
+            icon={<ThumbUpOutlined />}
+          >
+            Delete all your votes. This will impact any session you participated
+            on.
+          </DeleteItem>
+        </List>
+      </DialogContent>
     </Dialog>
+  );
+}
+
+type DeleteItemProps = {
+  disabled?: boolean;
+  checked: boolean;
+  icon: React.ReactNode;
+  onToggle?: (value: boolean) => void;
+};
+
+function DeleteItem({
+  children,
+  disabled,
+  icon,
+  checked,
+  onToggle,
+}: React.PropsWithChildren<DeleteItemProps>) {
+  return (
+    <ListItem>
+      <ListItemIcon>{icon}</ListItemIcon>
+      <ListItemText primary={children} />
+      <ListItemSecondaryAction>
+        <Switch
+          edge="end"
+          disabled={disabled}
+          checked={checked}
+          onChange={(_, v) => (onToggle ? onToggle(v) : noop)}
+        />
+      </ListItemSecondaryAction>
+    </ListItem>
   );
 }
