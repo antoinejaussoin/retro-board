340ec9734dbfa429d43de6b17f5e90145103eb15
Antoine Jaussoin <antoine@jaussoin.com>
2022-12-27 14:51:52 +0100

    TypeOrm upgrade migrations

--- backend/scripts/create-empty-migration.sh
+++ backend/scripts/create-empty-migration.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-./node_modules/.bin/typeorm-ts-node-commonjs -d src/db/index.ts migration:create src/db/migrations/$1
+./node_modules/.bin/typeorm-ts-node-commonjs migration:create src/db/migrations/$1
