3270dcb088b4499818649cd402d61e9767c9bbbd
Antoine Jaussoin <antoine@jaussoin.com>
2023-02-04 13:06:44 +0000

    move footer icon

--- frontend/src/views/game/footer/GameFooter.tsx
+++ frontend/src/views/game/footer/GameFooter.tsx
@@ -85,7 +85,7 @@ function GameFooter({
           <Button
             onClick={handleReady}
             variant="outlined"
-            endIcon={
+            startIcon={
               isUserReady ? (
                 <Create htmlColor={colors.orange[500]} />
               ) : (
