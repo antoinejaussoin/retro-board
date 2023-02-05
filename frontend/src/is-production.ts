76146f3a7262e2a8923b2fad229c3c3fd84d9b08
Antoine Jaussoin <antoine@jaussoin.com>
2023-02-05 13:39:51 +0000

    Remove logs

--- frontend/src/is-production.ts
+++ frontend/src/is-production.ts
@@ -1,6 +1,3 @@
 export function isProduction(): boolean {
-  console.log('NODE_ENV: ', process.env.NODE_ENV);
-  console.log('MODE: ', import.meta.env.MODE);
-  console.log('PROD: ', import.meta.env.PROD);
   return import.meta.env.PROD;
 }
