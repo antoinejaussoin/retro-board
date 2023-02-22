79763a0823b6bf0b9c8ac0b2b402fa60efa65dba
Antoine Jaussoin <antoine@jaussoin.com>
2023-02-22 15:57:43 +0000

    Clean up

--- marketing/src/typings.d.ts
+++ marketing/src/typings.d.ts
@@ -1,3 +1,15 @@
+import 'react-i18next';
+import common from '../public/locales/en/common.json';
+
 declare module '@redq/reuse-modal';
 declare module 'react-reveal';
 declare module 'react-reveal/Fade';
+
+declare module 'react-i18next' {
+  interface CustomTypeOptions {
+    defaultNS: 'common';
+    resources: {
+      common: typeof common;
+    };
+  }
+}
