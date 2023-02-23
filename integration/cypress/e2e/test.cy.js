0a8603300f8b35690bc28c4c5cdd66b98584ab5f
Antoine Jaussoin <antoine@jaussoin.com>
2023-02-23 22:08:20 +0000

    Fix e2e

--- integration/cypress/e2e/test.cy.js
+++ integration/cypress/e2e/test.cy.js
@@ -23,7 +23,7 @@ describe('Post workflow', () => {
     // Close cookie banner
     cy.get('.wpcc-btn').click();
     
-    get('login-button').click();
+    // get('login-button').click();
     get('anon-tab').click();
     get('anon-input').focus().type('Zelensky');
     get('anon-login-button').click();
@@ -50,7 +50,7 @@ describe('Post workflow', () => {
     // Close cookie banner
     cy.get('.wpcc-btn').click();
 
-    get('login-button').click();
+    // get('login-button').click();
     get('anon-tab').click();
     get('anon-input').focus().type('Zelensky');
     get('anon-login-button').click();
@@ -85,7 +85,7 @@ describe('Post workflow', () => {
       cy.get('.wpcc-btn').click();
 
       // Login
-      get('login-button').click();
+      // get('login-button').click();
 
       // Select the account tab
       get('account-tab').click();
