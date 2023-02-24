a62cb9e6d1cce0b5445df974ea5152a361acdb2a
Antoine Jaussoin <antoine@jaussoin.com>
2023-02-24 10:51:47 +0000

    Another try

--- marketing/next-i18next.config.js
+++ marketing/next-i18next.config.js
@@ -4,30 +4,6 @@ module.exports = {
     locales: ['en', 'fr', 'de', 'nl'],
     domains: [
       {
-        domain: 'retrospected.com',
-        defaultLocale: 'en',
-      },
-      {
-        domain: 'retrospected.fr',
-        defaultLocale: 'fr',
-      },
-      {
-        domain: 'retrospected.de',
-        defaultLocale: 'de',
-      },
-      {
-        domain: 'www.retrospected.com',
-        defaultLocale: 'en',
-      },
-      {
-        domain: 'www.retrospected.fr',
-        defaultLocale: 'fr',
-      },
-      {
-        domain: 'www.retrospected.de',
-        defaultLocale: 'de',
-      },
-      {
         domain: 'www-beta.retrospected.com',
         defaultLocale: 'en',
       },
