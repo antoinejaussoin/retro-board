01c2ba431ab7e594c4bc94f2da7b98f9e8390f5c
Antoine Jaussoin <antoine@jaussoin.com>
2023-02-24 09:53:57 +0000

    Adding domains

--- marketing/next-i18next.config.js
+++ marketing/next-i18next.config.js
@@ -2,5 +2,19 @@ module.exports = {
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'fr', 'de', 'nl'],
+    domains: [
+      {
+        domain: 'retrospected.com',
+        defaultLocale: 'en',
+      },
+      {
+        domain: 'retrospected.fr',
+        defaultLocale: 'fr',
+      },
+      {
+        domain: 'retrospected.de',
+        defaultLocale: 'de',
+      }
+    ],
   },
 }
