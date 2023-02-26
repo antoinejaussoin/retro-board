c1a328258e2f65b4d43c1b7abb83891a879152b3
Antoine Jaussoin <antoine@jaussoin.com>
2023-02-26 11:58:49 +0000

    Another go

--- integration/cypress/e2e/test.cy.js
+++ integration/cypress/e2e/test.cy.js
@@ -1,36 +1,32 @@
+const timeout = +Cypress.env('backend_delay');
+
 describe('Home Page', () => {
   it('Should load correctly', () => {
     cy.visit('/');
 
-    cy.wait(+Cypress.env('backend_delay'));
-  
-    cy.get('#content').get('h1').should('contain', 'Welcome!');
+    cy.get('#content').get('h1', { timeout }).should('contain', 'Welcome!');
   });
 });
 
 
 function get(name, rest) {
-  return cy.get(`[data-cy=${name}]${rest ?? ''}`);
+  return cy.get(`[data-cy=${name}]${rest ?? ''}`, { timeout });
 }
 
-
-
 describe('Post workflow', () => {
+  beforeEach(() => {
+    cy.setCookie('wpcc', 'dismiss');
+  });
+
   it('Should login and write a post', () => {
     cy.visit('/');
-
-    // We need to wait until the backend is ready
-    cy.wait(+Cypress.env('backend_delay'));
-
-    // Close cookie banner
-    cy.get('.wpcc-btn').click();
     
     get('anon-tab').click();
     get('anon-input').focus().type('Zelensky');
     get('anon-login-button').click();
 
     // Home page should display the user name
-    cy.get('#content').should('contain', 'Welcome, Zelensky');
+    cy.get('#content', { timeout }).should('contain', 'Welcome, Zelensky');
 
     // And then allow creating a new session
     get('new-session-button').click();
@@ -41,26 +37,19 @@ describe('Post workflow', () => {
     // Reload the page
     cy.reload();
 
-    cy.wait(+Cypress.env('backend_delay'));
-
     // The post should still be there
-    cy.get('#content').should('contain', 'Slava Ukraini!');
+    cy.get('#content', {timeout}).should('contain', 'Slava Ukraini!');
   });
 
   it('Should change language and translate the app', () => {
     cy.visit('/');
-
-    cy.wait(+Cypress.env('backend_delay'));
-
-    // Close cookie banner
-    cy.get('.wpcc-btn').click();
 
     get('anon-tab').click();
     get('anon-input').focus().type('Zelensky');
     get('anon-login-button').click();
 
     // Home page should display the user name
-    cy.get('#content').should('contain', 'Welcome, Zelensky');
+    cy.get('#content', { timeout }).should('contain', 'Welcome, Zelensky');
 
     // Change language
     get('side-panel-toggle').click();
@@ -68,10 +57,10 @@ describe('Post workflow', () => {
     get('language-picker-item-fr-FR').click();
 
     // Exit panel
-    cy.get('body').type('{esc}');
+    cy.get('body', {timeout }).type('{esc}');
 
     // Home page should now be in French
-    cy.get('#content').should('contain', 'Bienvenue, Zelensky');
+    cy.get('#content', {timeout }).should('contain', 'Bienvenue, Zelensky');
 
     // Logout
     get('account-menu').click();
@@ -80,15 +69,9 @@ describe('Post workflow', () => {
   });
 
   it('Should be able to create a new account', () => {
-    cy.session('create-account', () => {
       const id = Date.now();
 
       cy.visit('/');
-
-      cy.wait(+Cypress.env('backend_delay'));
-
-      // Close cookie banner
-      cy.get('.wpcc-btn').click();
 
       // Select the account tab
       get('account-tab').click();
@@ -109,28 +92,34 @@ describe('Post workflow', () => {
 
       // And write a post
       get('column-input').first().focus().type('Slava Ukraini!{enter}');
-      cy.get('#content').should('contain', 'Slava Ukraini!');
+      cy.get('#content', {timeout }).should('contain', 'Slava Ukraini!');
 
       // And some chat
       get('open-chat-button').click({force: true});
       get('chat-input').focus().type('This is a message{enter}');
-      cy.get('#content').should('contain', 'This is a message');
+      cy.get('#content', {timeout }).should('contain', 'This is a message');
     
       // Close
       get('open-chat-button').click({force: true});
-    });
+
+       // Go to the user admin and delete the account
+       get('account-menu').click();
+       get('account-menu-account').click();
+       get('delete-account-button').click();
+       get('delete-modal-sessions').click();
+       get('delete-modal-posts').click();
+       get('delete-modal-votes').click();
+       get('delete-modal-delete-button').click();
+       get('delete-modal-confirm').click();
+ 
+       // We should be back to the home page
+       cy.get('body', {timeout}).get('h1').should('contain', 'Welcome!');
   });
 
   it('Should be able to delete an account', () => {
-    cy.session('delete-account', () => {
       const id = Date.now();
 
       cy.visit('/');
-
-      cy.wait(+Cypress.env('backend_delay'));
-
-      // Close cookie banner
-      cy.get('.wpcc-btn').click();
 
       // Select the account tab
       get('account-tab').click();
@@ -146,8 +135,6 @@ describe('Post workflow', () => {
       // Register
       get('register-button').click();
 
-      cy.wait(+Cypress.env('backend_delay'));
-
       // Go to the user admin and delete the account
       get('account-menu').click();
       get('account-menu-account').click();
@@ -159,7 +146,6 @@ describe('Post workflow', () => {
       get('delete-modal-confirm').click();
 
       // We should be back to the home page
-      cy.get('body').get('h1').should('contain', 'Welcome!');
-    });
+      cy.get('body', {timeout}).get('h1').should('contain', 'Welcome!');
   });
 });
