7e9694ff67ecafeabed1dcc1e872c1688417b489
Antoine Jaussoin <antoine@jaussoin.com>
2023-03-01 10:48:12 +0000

    Improve demo

--- backend/src/db/actions/demo.ts
+++ backend/src/db/actions/demo.ts
@@ -1,16 +1,44 @@
 import { v4 } from 'uuid';
-import { Session } from '../../common/types.js';
+import { Post, Session } from '../../common/types.js';
 import { UserEntity } from '../../db/entities/UserIdentity.js';
-import { savePost } from './posts.js';
-import { createSession } from './sessions.js';
+import { savePost, saveVote } from './posts.js';
+import { createSession, getSession, saveSession } from './sessions.js';
 import { getNext, getMiddle } from '../../lexorank.js';
+import { registerAnonymousUser } from './users.js';
+import { DeepPartial } from 'typeorm';
 
 export async function createDemoSession(author: UserEntity): Promise<Session> {
   const session = await createSession(author);
+  session.name = 'My Retro Demo';
+  session.options = {
+    ...session.options,
+    allowActions: true,
+    allowGiphy: true,
+    allowMultipleVotes: true,
+    allowReordering: true,
+    allowSelfVoting: true,
+    allowTimer: true,
+    allowCancelVote: true,
+    allowGrouping: true,
+    maxDownVotes: 20,
+    maxUpVotes: 20,
+    readonlyOnTimerEnd: true,
+  };
+  await saveSession(author.id, session);
   let rank = getMiddle();
 
-  async function createPost(content: string, column: number) {
-    await savePost(author.id, session.id, {
+  const otherUser = (await registerAnonymousUser('John Doe^' + v4(), v4()))!;
+
+  const otherUserId = otherUser!.user!.id;
+
+  async function createPost(
+    content: string,
+    column: number,
+    votes = 0,
+    own = false
+  ) {
+    rank = getNext(rank);
+    const postData: DeepPartial<Post> = {
       content,
       column,
       giphy: null,
@@ -19,14 +47,41 @@ export async function createDemoSession(author: UserEntity): Promise<Session> {
       votes: [],
       group: null,
       id: v4(),
-    });
-    rank = getNext(rank);
+    };
+    const post = (await savePost(
+      own ? author.id : otherUserId,
+      session.id,
+      postData
+    ))!;
+    for (let i = 0; i < votes; i++) {
+      saveVote(otherUserId, '', post.id, {
+        id: v4(),
+        type: 'like',
+        user: otherUser.user.toJson(),
+      });
+    }
+    const updatedSession = await getSession(session.id);
+    return updatedSession;
   }
 
   await Promise.all([
-    createPost('I am enjoying our new retrospective board!', 0),
+    createPost('I am enjoying our new retrospective board!', 0, 5),
     createPost('I loved how we can vote on posts!', 0),
-    createPost('Where should I go on vacation?', 1),
+    createPost('I wish I discovered this tool sooner', 1, 2, true),
+    createPost(
+      'Have you tried different settings? Click on "Customise" to see what you can do.',
+      2
+    ),
+    createPost(
+      'Have you tried Giphy by clicking on the yellow smiley face?',
+      2,
+      1,
+      true
+    ),
+    createPost(
+      'You can also share this URL with somebody else to collaborate on the same board.',
+      2
+    ),
   ]);
 
   return session;
